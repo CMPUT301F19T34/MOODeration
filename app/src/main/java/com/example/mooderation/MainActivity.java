71a8213daa60c677ad8b642529f96b9c10598394
rkluzinski <ryankluzinski@gmail.com>
2019-11-06 14:25:29 -0700

    Add google-services.json to .gitignore Add Navigation Drawer to layout

--- app/src/main/java/com/example/mooderation/MainActivity.java
+++ app/src/main/java/com/example/mooderation/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
@@ -14,6 +21,9 @@ import com.google.firebase.auth.FirebaseAuth;
  * implementation in fragments.
  */
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,19 @@ public class MainActivity extends AppCompatActivity {
                 "user"
         ));
         Log.e("TAG", "Setting a viewmodel");
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 }

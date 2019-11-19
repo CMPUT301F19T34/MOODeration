1f371974f3db182afa23b7c5a55f7c36c6c21e2a
rkluzinski <ryankluzinski@gmail.com>
2019-11-19 09:03:55 -0700

    Update MoodHistory and FollowRequest ViewModel tests

--- app/src/androidTest/java/com/example/mooderation/viewmodel/FollowRequestsViewModelTest.java
+++ app/src/androidTest/java/com/example/mooderation/viewmodel/FollowRequestsViewModelTest.java
@@ -1,8 +1,6 @@
 package com.example.mooderation.viewmodel;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 
 import com.example.mooderation.FollowRequest;
 import com.example.mooderation.backend.FollowRepository;
@@ -13,24 +11,17 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class FollowRequestsViewModelTest {
     @Mock
     FollowRepository followRepository;
     @Mock
     FollowRequest request;
-    @Mock
-    Observer<List<FollowRequest>> followRequestObserver;
 
     private FollowRequestsViewModel followRequestsViewModel;
-    private MutableLiveData<List<FollowRequest>> followRequests = new MutableLiveData<>();
 
     // forces tasks to execute synchronously
     @Rule
@@ -39,34 +30,20 @@ public class FollowRequestsViewModelTest {
     @Before
     public void setUp() throws ExecutionException, InterruptedException {
         MockitoAnnotations.initMocks(this);
-
         followRequestsViewModel = new FollowRequestsViewModel(followRepository);
-        when(followRepository.getFollowRequests()).thenReturn(followRequests);
-
-        followRequestsViewModel.getFollowRequests().observeForever(followRequestObserver);
     }
 
     // TODO replace with better tests
 
     @Test
     public void testAcceptFollowRequest() throws ExecutionException, InterruptedException {
-        when(followRepository.accept(any())).then(invocation -> {
-            followRequests.setValue(new ArrayList<>());
-            return null;
-        });
-
         followRequestsViewModel.acceptRequest(request);
-        verify(followRequestObserver).onChanged(new ArrayList<>());
+        verify(followRepository).accept(request);
     }
 
     @Test
     public void testDenyFollowRequest() throws ExecutionException, InterruptedException {
-        when(followRepository.deny(any())).then(invocation -> {
-            followRequests.setValue(new ArrayList<>());
-            return null;
-        });
-
         followRequestsViewModel.denyRequest(request);
-        verify(followRequestObserver).onChanged(new ArrayList<>());
+        verify(followRepository).deny(request);
     }
 }
--- app/src/androidTest/java/com/example/mooderation/viewmodel/MoodHistoryViewModelTest.java
+++ app/src/androidTest/java/com/example/mooderation/viewmodel/MoodHistoryViewModelTest.java
@@ -1,8 +1,6 @@
 package com.example.mooderation.viewmodel;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 
 import com.example.mooderation.MoodEvent;
 import com.example.mooderation.backend.MoodRepository;
@@ -15,13 +13,9 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class MoodHistoryViewModelTest {
@@ -29,11 +23,8 @@ public class MoodHistoryViewModelTest {
     MoodRepository moodRepository;
     @Mock
     MoodEvent moodEvent;
-    @Mock
-    Observer<List<MoodEvent>> moodHistoryObserver;
 
     private MoodHistoryViewModel moodHistoryViewModel;
-    private MutableLiveData<List<MoodEvent>> moodHistory = new MutableLiveData<>();
 
     // forces tasks to execute synchronously
     @Rule
@@ -42,34 +33,18 @@ public class MoodHistoryViewModelTest {
     @Before
     public void setUp() throws ExecutionException, InterruptedException {
         MockitoAnnotations.initMocks(this);
-
         moodHistoryViewModel = new MoodHistoryViewModel(moodRepository);
-        when(moodRepository.getMoodHistory()).thenReturn(moodHistory);
-
-        moodHistoryViewModel.getMoodHistory().observeForever(moodHistoryObserver);
     }
 
-    // TODO replace with better tests
-
     @Test
-    public void testAddMoodEvent() throws ExecutionException, InterruptedException {
-        when(moodRepository.add(any())).then(invocation -> {
-            moodHistory.setValue(new ArrayList<>());
-            return null;
-        });
-
+    public void testAddMoodEvent() {
         moodHistoryViewModel.addMoodEvent(moodEvent);
-        verify(moodHistoryObserver).onChanged(new ArrayList<>());
+        verify(moodRepository).add(moodEvent);
     }
 
     @Test
-    public void testDeleteMoodEvent() throws ExecutionException, InterruptedException {
-        when(moodRepository.remove(any())).then(invocation -> {
-            moodHistory.setValue(new ArrayList<>());
-            return null;
-        });
-
+    public void testDeleteMoodEvent() {
         moodHistoryViewModel.removeMoodEvent(moodEvent);
-        verify(moodHistoryObserver).onChanged(new ArrayList<>());
+        verify(moodRepository).remove(moodEvent);
     }
 }
